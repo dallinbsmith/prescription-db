c54db84c8ba6cc9a279a6834513626ad849efbbd
Dallin Smith <dallinbsmith@gmail.com>
2026-02-19 12:32:21 -0700

    Fix ESM compatibility in migrate script

--- database/migrate.ts
+++ database/migrate.ts
@@ -1,7 +1,11 @@
 import { Pool } from 'pg';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, '../backend/.env') });
 
